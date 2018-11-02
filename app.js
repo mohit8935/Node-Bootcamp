81b5e4b79e61bec3696b45fe61be8f5acbf2e547
Mohit Nihalani <nihalanim9@gmail.com>
2018-11-02 15:58:35 +0530

    added campground post method

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 var express = require("express")
 var app =  express();
+var bodyParser = require("body-parser")
 const port = 3000
-
+var campgrounds = [
+    {name: "James Bond", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f1c078a5eeb5b0_340.jpg"},
+    {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144594f1c07ba4edbd_340.jpg"}
+]
+// using bodyparser with Express
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 app.get("/", function(req,res){
     res.render("landing")
 });
 app.get("/campgrounds", function(req,res){
-    var campgrounds = [
-        {name: "James Bond", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f1c078a5eeb5b0_340.jpg"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144594f1c07ba4edbd_340.jpg"}
-    ]
+    
     res.render("campgrounds", {campgrounds:campgrounds});
 
 });
 app.listen(port, function(){
     console.log("The bootcamp server has started")
+});
+
+
+app.get("/campgrounds/new", function(req,res){
+    res.render("new.ejs");
+});
+app.post("/campgrounds", function(req,res){
+
+    var name =  req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+    // adding data to campgrounds array
+    // redirecting back to campgrounds.page
 });
