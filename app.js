a41ff763e2d63a3f689bae966b14ba7cffde8a6b
Mohit Nihalani <nihalanim9@gmail.com>
2018-11-03 18:06:50 +0530

    added seed class and comment model and its associations

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express     = require("express"),
     mongoose    = require('mongoose'),
     bodyParser  = require("body-parser")
     Campground  = require("./models/campground")
+    Comment     = require("./models/comment")
+    seedDB      = require("./seeds")
 
+seedDB();
 mongoose.connect('mongodb://localhost/campground')
 
 var db = mongoose.connection;
@@ -11,25 +14,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
-
-
-
-/* campground1 = new Campground({name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f1c070a1e4b5be_340.jpg", description: "Salmon Creek is located in southwestern Clark County at 45°42′18″N 122°39′41″W (45.704938, -122.661300).[4] It is bordered to the northeast by Mount Vista, to the east by Barberton, to the southeast by Walnut Grove, to the south by Hazel Dell, to the southwest by Lake Shore, and to the west by Felida. Downtown Vancouver is 6 miles (10 km) to the south. According to the United States Census Bureau, the Salmon Creek CDP has a total area of 6.3 square miles (16.4 km2), all of it land"});
-Campground.create(campground1, function(err, newlycreated){
-    if(err){
-        console.log(err)
-    } else {
-        console.log("added");
-    }
-});
-campground2 = new Campground({name: "Mountain Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f1c070a1e4b5be_340.jpg", description: "A mountain is a large landform that rises above the surrounding land in a limited area, usually in the form of a peak. A mountain is generally steeper than a hill. Mountains are formed through tectonic forces or volcanism. ... A few mountains are isolated summits, but most occur in huge mountain ranges."});
-Campground.create(campground2, function(err, newlycreated){
-    if(err){
-        console.log(err)
-    } else {
-        console.log("added");
-    }
-}); */
 
 
 const port = 3000
@@ -82,8 +66,7 @@ app.post("/campgrounds", function(req,res){
 app.get("/campgrounds/:id", function (req,res) {
     var campground_id = req.params.id
     console.log(campground_id)
-    Campground
-    .findOne({_id: req.params.id}, function (err, foundCampground){
+    Campground.findById(campground_id).populate("comments").exec(function (err, foundCampground){
       res.render("show", 
       {campground:foundCampground});
       
