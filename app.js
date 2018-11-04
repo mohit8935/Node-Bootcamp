1ed4773b84fa0c612febbdc4001c5306689ec68b
Mohit Nihalani <nihalanim9@gmail.com>
2018-11-04 13:44:39 +0530

    added routes for Sign UP

--- app.js
+++ app.js
@@ -133,3 +133,17 @@ app.get("/register", function(req,res){
     res.render("register");
 
 });
+
+app.post("/register", function(req,res){
+    var newUser =  new User({username: req.body.username});
+    User.register(newUser,req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/campgrounds")
+        });
+    });
+
+});
